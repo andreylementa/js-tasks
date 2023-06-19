79df804f4f337ff96e8c7f9f282291af3c409f0a
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-19 20:51:17 +0300

    task 2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Task 1 -------------------
-const arrOfNumbers = [1, 10, 31, 20, 33, 4];
-const sumArr = arrOfNumbers.reduce((acc, number) => {
-  return acc + number;
-}, 0);
+//const arrOfNumbers = [1, 10, 31, 20, 33, 4];
+//const sumArr = arrOfNumbers.reduce((acc, number) => {
+//  return acc + number;
+//}, 0);
 
-console.log(sumArr);
+//console.log(sumArr);
+
+// Task 2 -------------------
+
+const arrOfStrings = ["asdasdasd", "dddsssaa", "asdasdasd", "fffrfefasd"];
+
+const arrOfStryingsUpper = arrOfStrings.map((item) => item.toUpperCase());
+console.log(arrOfStryingsUpper);
