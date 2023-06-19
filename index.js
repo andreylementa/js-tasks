f384aea20ffdbafb8603e245af15a32f993d33c6
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-19 20:54:18 +0300

    task 3

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
 
 // Task 2 -------------------
 
-const arrOfStrings = ["asdasdasd", "dddsssaa", "asdasdasd", "fffrfefasd"];
+//const arrOfStrings = ["asdasdasd", "dddsssaa", "asdasdasd", "fffrfefasd"];
 
-const arrOfStryingsUpper = arrOfStrings.map((item) => item.toUpperCase());
-console.log(arrOfStryingsUpper);
+//const arrOfStryingsUpper = arrOfStrings.map((item) => item.toUpperCase());
+//console.log(arrOfStryingsUpper);
+
+// Task 2 -------------------
+
+const arrOfNumbers = [1, 10, 31, 20, 33, 4];
+const arrOfEven = arrOfNumbers.filter((item) => item % 2 === 0);
+console.log(arrOfEven);
