0627ba398d6cfce9d2ea498876fd6525dc352fec
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-20 14:59:29 +0300

    tasks

--- index.js
+++ index.js
@@ -174,13 +174,101 @@
 
 // Task 18 -------------------
 
-const arrOfObjects = [
-  { name: "Breu", age: 11 },
-  { name: "Alex", age: 34 },
-  { name: "Jack", age: 26 },
-  { name: "Martin", age: 47 },
-  { name: "Stiv", age: 10 },
-  { name: "Bob", age: 55 },
-];
+//const arrOfObjects = [
+//  { name: "Breu", age: 11 },
+//  { name: "Alex", age: 34 },
+//  { name: "Jack", age: 26 },
+//  { name: "Martin", age: 47 },
+//  { name: "Stiv", age: 10 },
+//  { name: "Bob", age: 55 },
+//];
 
-console.log(arrOfObjects.some((item) => item.age > 50));
+//console.log(arrOfObjects.some((item) => item.age > 50));
+
+// Task 2_2
+
+const summElementsWorker = function (...items) {
+  if (items.length === 0) {
+    return 0;
+  } else {
+    return items.reduce((acc, item) => acc + item, 0);
+  }
+};
+summElementsWorker(1, 2, 3, 4, 5);
+
+const differenceMaxMinWorker = function (...items) {
+  if (items.length === 0) {
+    return 0;
+  } else {
+    //const minValue = items.sort((a, b) => (a < b ? 1 : -1)).slice(-1)[0];
+    const minValue = Math.min.apply(null, items);
+    //const maxValue = items.sort((a, b) => (a > b ? 1 : -1)).slice(-1)[0];
+    const maxValue = Math.max.apply(null, items);
+    return maxValue - minValue;
+  }
+};
+
+differenceMaxMinWorker(4, 5, 6, 3, 1, 10);
+
+const differenceEvenOddWorker = function (...items) {
+  if (items.length === 0) {
+    return 0;
+  } else {
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+    items.forEach((item) => {
+      item % 2 === 0 ? (sumEvenElement += 1) : (sumOddElement += 1);
+    });
+    return sumEvenElement - sumOddElement;
+  }
+};
+
+differenceEvenOddWorker(1, 2, 44, 6, 4, 5);
+
+const averageEvenElementsWorker = function (...items) {
+  if (items.length === 0) {
+    return 0;
+  } else {
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+    items.forEach((item) => {
+      if (item % 2 === 0) {
+        sumEvenElement += item;
+        countEvenElement += 1;
+      }
+    });
+    return sumEvenElement / countEvenElement;
+  }
+};
+averageEvenElementsWorker();
+//averageEvenElementsWorker(1, 3, 4, 6, 9, 10, 2, 3);
+
+const makeWork = function (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  arrOfArr.forEach((arr) => {
+    const resultValue = func(...arr);
+    resultValue > maxWorkerResult
+      ? (maxWorkerResult = resultValue)
+      : maxWorkerResult;
+  });
+  console.log(maxWorkerResult);
+};
+
+makeWork(
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 1, 2],
+    [44, 22, 11],
+  ],
+  summElementsWorker
+);
+
+// Task Formater numbers -----------------------
+const parseCount = function (value) {
+  if (Number.parseFloat(value)) {
+    return value;
+  } else {
+    console.log("Невалидное значение");
+  }
+};
